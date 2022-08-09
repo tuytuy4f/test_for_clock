c4faf460ab7767656c7fbbd8df5c2fd25df0196c
tuytuy4f <109992516+tuytuy4f@users.noreply.github.com>
2022-08-09 11:46:48 -0300

    Delete get_ip.js

--- get_ip.js
+++
@@ -1,7 +0,0 @@
-<script type="application/javascript">
-  function getIP(json) {
-    document.write("My public IP address is: ", json.ip);
-  }
-</script>
-
-<script type="application/javascript" src="https://api.ipify.org?format=jsonp&callback=getIP"></script>
